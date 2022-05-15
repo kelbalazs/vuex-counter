d357694ec10c6290880f00c858571f2a08dd43e3
Balazs Kelemen <kelbal@gmail.com>
2022-05-15 10:03:56 +0100

    simple

---
+++ src/store/index.js
@@ -0,0 +1,44 @@
+import { createStore } from 'vuex'
+import axios from 'axios'
+
+export default createStore({
+  state: {
+    counter: 0,
+    colourCode: 'blue'
+  },
+  getters: {
+    counterSquared(state){
+      return state.counter * state.counter
+    }
+  },
+  mutations: {
+    
+      increaseCounter(state, randomNumber) {
+        state.counter+=randomNumber
+      },
+      decreaseCounter(state, randomNumber) {
+        state.counter-=randomNumber
+      },
+      setColourCode(state, newValue){
+        state.colourCode = newValue
+      }
+  },
+  actions: {
+    increaseCounter({ commit }){
+         axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response =>{
+       commit('increaseCounter',response.data)
+      })
+    },
+
+    decreaseCounter({ commit }){
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response =>{
+    commit('decreaseCounter',response.data)
+   })
+ },
+    setColourCode({commit}, newValue){
+      commit('setColourCode', newValue)
+    }
+  },
+  modules: {
+  }
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+<div
+    :style="{color: $store.state.colourCode}"
+   class="counter">
+  {{$store.state.counter}}
+</div>
+<div class="counter-squared">
+  {{$store.state.counter}}
+  <sup>2</sup> =
+  {{$store.getters.counterSquared}}
+</div>
+<div class="buttons">
+  <button @click="$store.dispatch('decreaseCounter')">-</button>
+  <button @click="$store.dispatch('increaseCounter')">+</button>
+</div>
+<input 
+  v-model="colourCode"
+  placeholder="Enter colour code"
+  type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  computed: {
+    colourCode:{
+      get() {
+        return this.$store.state.colourCode
+      },
+
+      set(newValue){
+        this.$store.dispatch('setColourCode', newValue)
+      }
+    }
+  }
+
+}
+
+
+</script>
+
+
+<style>
+  div {
+    margin-bottom: 10px;
+  }
+  .counter {
+    font-size: 80px;
+  }
+  .buttons button {
+    font-size: 40px;
+    width: 100px;
+    margin: 0 10 px;
+  }
+</style>
